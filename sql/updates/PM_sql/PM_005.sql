460bc551c852c1eb6db687cc032a7cc7a1955a84
zamalaev <zamalaev88@mail.ru>
2012-11-21 23:27:40 +0300

    [PM 005] fix Language Goblin and Worgen (Gilneas, Kezan)

---
+++ sql/updates/PM_sql/PM_005.sql
@@ -0,0 +1,17 @@
+REPLACE INTO `playercreateinfo_spell` (`race`, `class`, `Spell`, `Note`) VALUES
+(22, 1, 69270, 'Language Worgen'),
+(22, 3, 69270, 'Language Worgen'),
+(22, 4, 69270, 'Language Worgen'),
+(22, 5, 69270, 'Language Worgen'),
+(22, 6, 69270, 'Language Worgen'),
+(22, 8, 69270, 'Language Worgen'),
+(22, 9, 69270, 'Language Worgen'),
+(22, 11, 69270, 'Language Worgen'),
+(9, 1, 69269, 'Language Goblin'),
+(9, 3, 69269, 'Language Goblin'),
+(9, 4, 69269, 'Language Goblin'),
+(9, 5, 69269, 'Language Goblin'),
+(9, 6, 69269, 'Language Goblin'),
+(9, 7, 69269, 'Language Goblin'),
+(9, 8, 69269, 'Language Goblin'),
+(9, 9, 69269, 'Language Goblin');
